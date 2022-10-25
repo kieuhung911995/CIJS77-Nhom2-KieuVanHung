60dddda09054be5780f97f634b897435e6386c44
kieuhung911995 <110046293+kieuhung911995@users.noreply.github.com>
2022-10-25 12:36:27 +0700

    BTVN3

--- src/App.jsx
+++ src/App.jsx
@@ -2,162 +2,109 @@ import React from "react";
 import "./index.css";
 const data1 = [
   {
-    src: "https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2020/12/17/863059/Osad-2.png",
-    title: "Lời có cánh",
-    artist: "OSAD",
-    timeUp: "2 giờ trước",
+    number: "1",
+    src:'https://d3t3ozftmdmh3i.cloudfront.net/production/podcast_uploaded_episode400/21024180/21024180-1665924329601-f6d68382842a7.jpg',
+    title: "#26 Bí quyết thành công của người Do Thái | Đắp chăn nằm nghe...",
+    chanel: "Đắp chăn nằm nghe Tun kể",
+    timeUp: "16/10/2022 - 16 phút"
   },
   {
-    src: "http://cdn.shopify.com/s/files/1/0546/6033/products/8817584_6330_1.jpg?v=1654026154",
-    title: "Lời có cánh",
-    artist: "Ellie Gounding",
-    timeUp: "1 ngày trước",
+    number: "2",
+    src:'https://i.ytimg.com/vi/uZCKUQbrkaQ/maxresdefault.jpg',
+    title: "#25 Cùng Đích Lép đi tìm 5 dấu hiệu lý tưởng mà một người bạn đời...",
+    chanel: "Đắp chăn nằm nghe Tun kể",
+    timeUp: "09/10/2022 - 24 phút"
   },
   {
-    src: "https://vnw-img-cdn.popsww.com/api/v2/containers/file2/cms_topic/masew_07_horizontalposter-c1a5347d7910-1631606018825-efo8ePlA.jpg?v=0&maxW=600",
-    title: "Hối Duyên",
-    artist: "Masew, Khoi Vu, Great",
-    timeUp: "4 ngày trước",
-  },
-  {
-    src: "https://photo-resize-zmp3.zmdcdn.me/w360_r1x1_jpeg/avatars/2/c/6/5/2c651dc58c046d51175a6b38105503c5.jpg",
-    title: "Nỗi nhớ khi xưa",
-    artist: "Kha",
-    timeUp: "Hôm nay",
-  },
-  {
-    src: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTgwMTgyMTgzOTk3MDg4ODkw/gettyimages-483195065.jpg",
-    title: "Monotonia",
-    artist: "Shakỉa, Ozuna",
-    timeUp: "1 ngày trước",
-  },
-  {
-    src: "https://avatar-ex-swe.nixcdn.com/singer/avatar/2018/03/06/1/0/a/4/1520308951432_600.jpg",
-    title: "Người còn thương",
-    artist: "Minh Vương M4U, Thương Võ, ACV",
-    timeUp: "5 ngày trước",
-  },
-  {
-    src: "https://znews-photo.zingcdn.me/w660/Uploaded/wpdhnwhnw/2022_10_14/307653264_637995611013005_2590676343667179460_n_1.jpg",
-    title: "Cô đơn trên sofa",
-    artist: "Hồ Ngọc Hà",
-    timeUp: "1 ngày trước",
-  },
-  {
-    src: "https://static2.yan.vn/YanNews/2167221/202107/wren-evans-la-ai-thong-tin-tieu-su-anh-chang-nhu-the-nao-bc1c132f.jpg",
-    title: "Vàng",
-    artist: "Daisy Le Garcon, Wren Evans",
-    timeUp: "1 ngày trước",
-  },
-  {
-    src: "https://nguoinoitieng.tv/images/nnt/102/1/bgbd.jpg",
-    title: "Hoa tình chẳng nở",
-    artist: "Dee Trần",
-    timeUp: "5 ngày trước",
-  },
-  {
-    src: "https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2020/12/17/863059/Osad-2.png",
-    title: "90609",
-    artist: "Tóc tiên, Mew Amazing",
-    timeUp: "1 ngày trước",
-  },
-  {
-    src: "https://bachkhoawiki.com/wp-content/uploads/2021/07/anh-tu-atus.jpg",
-    title: "Bài này không để đi diễn",
-    artist: "Anh Tú Atus",
-    timeUp: "3 ngày trước",
-  },
-  {
-    src: "https://vcdn1-giaitri.vnecdn.net/2022/07/18/vucattuongtop-1658133596-9601-1658133904.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=j7NHo_5IysIPr-I1IE4k4g",
-    title: "HUNGGOVER",
-    artist: "Minh, Vũ Cát Tường",
-    timeUp: "2 giờ trước",
+    number: "3",
+    src:'https://storage.buzzsprout.com/variants/s6tvepoexorp5nqr22vd4018fjmw/60854458c4d1acdf4e1c2f79c4137142d85d78e379bdafbd69bd34c85f5819ad.jpg',
+    title: "[Phần 2] Cuộc Đời Nói Chuyện Bằng Kết Quả - Tri kỷ cảm xúc #252",
+    chanel: "Tri kỷ cảm xúc",
+    timeUp: "17/10/2022 - 25 phút"
   },
 ];
 const data2 = [
   {
-    src: "https://media.istockphoto.com/vectors/wednesday-handdrawn-lettering-spring-vector-poster-floral-frame-vector-id1142211001?k=20&m=1142211001&s=612x612&w=0&h=nCm1zDvqmbxU7L7o_uFyfONTcvZKFHM8_-Fm92jS6Is=",
-    title: "Nhạc Cho Thứ Tư",
-    note: "Thứ Tư từ từ tận hưởng Indie Việt",
+    number: "4",
+    src:'https://i.scdn.co/image/ab6765630000ba8a5b61945f5f6bf820d3d43fbe',
+    title: "85. Hành Trình Hiểu Về Bản Thân",
+    chanel: "HIEU.TV",
+    timeUp: "11/10/2022 - 19 phút"
   },
   {
-    src: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/c/8/1/1/c81185776ffb408b0edb6acf53756abb.jpg",
-    title: "Đóa Hồng Nhạc Việt",
-    note: "Nữ ca sĩ tỏa sắc hương trong khu vườn V-Pop",
+    number: "5",
+    src:'https://i.scdn.co/image/ab6765630000ba8a5b61945f5f6bf820d3d43fbe',
+    title: "86. Hướng Nội và Hướng Ngoại",
+    chanel: "HIEU.TV",
+    timeUp: "21/10/2022 - 22 phút"
   },
   {
-    src: "https://avatar-ex-swe.nixcdn.com/playlist/2020/04/22/9/3/4/c/1587551129349.jpg",
-    title: "Chillout",
-    note: "Bạn đã sẵn sàng bỏ hết mọi ưu phiền mệt mỏi hay chưa",
-  },
-  {
-    src: "https://i1.sndcdn.com/artworks-001011856801-99r2ni-t500x500.jpg",
-    title: "Pop Rising",
-    note: "Tiềm năng Pop Âu Mỹ đáng nghe nhất hiện tại",
-  },
-  {
-    src: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/a/2/8/2/a282a0d1ca6dff2bdba8f0944af98a64.jpg",
-    title: "Hà Nhi's Ex Story",
-    note: "Lâu lâu nhắc lại 'vũ trụ người yêu cũ' cùng Hà Nhi",
+    number: "6",
+    src:'https://i.scdn.co/image/ab6765630000ba8a89a4c1b4639678f20e146ad1',
+    title: "Cuối đoạn đường | Tập cuối - Nắng Thủy Tinh Podcast Series",
+    chanel: "Nắng Thủy Tinh",
+    timeUp: "21/08/2022 - 24 phút"
   },
 ];
-const SubApp1 = ({ src, title, artist, timeUp }) => {
+const SubApp1 = ({ number, src, title, chanel, timeUp }) => {
   return (
-    <div className="component1">
-      <img src={src} className="comp1-img"></img>
-      <ul className="comp1-text">
-        <li className="comp1-header">{title}</li>
-        <li className="comp1-p">{artist}</li>
-        <li className="comp1-p">{timeUp}</li>
+    <div className="component-item">
+      <div className="comp-header">{number}</div>
+      <img src={src} className="comp-img"></img>
+      <ul className="comp-text">
+        <li className="comp-header">{title}</li>
+        <li className="comp-p">{chanel}</li>
+        <li className="comp-p">{timeUp}</li>
       </ul>
     </div>
-  );
+  )
 };
-const SubApp2 = ({ src, title, note }) => {
+const SubApp2 = ({ number, src, title, chanel, timeUp }) => {
   return (
-    <div className="component2">
-      <img src={src} className="comp2-img"></img>
-      <ul className="comp2-text">
-        <li className="comp1-header">{title}</li>
-        <li className="comp1-p">{note}</li>
+    <div className="component-item">
+      <div className="comp-header">{number}</div>
+      <img src={src} className="comp-img"></img>
+      <ul className="comp-text">
+        <li className="comp-header">{title}</li>
+        <li className="comp-p">{chanel}</li>
+        <li className="comp-p">{timeUp}</li>
       </ul>
     </div>
-  );
+  )
 };
 const App = () => {
   return (
-    <div className="background">
-      <h1 className="menu-header">Mới phát hành</h1>
-      <ul className="menu-flex">
-        <div className="menu-item12">
-          <li className="menu-item1">
-            <div>BÀI HÁT</div>
-          </li>
-          <li className="menu-item2">ALBUM</li>
-        </div>
-        <li className="menu-item3">TẤT CẢ</li>
-      </ul>
-      <div className="main">
-        {data1.map((element) => (
-          <SubApp1
-            src={element.src}
-            title={element.title}
-            artist={element.artist}
-            timeUp={element.timeUp}
-          />
-        ))}
+    <div className="body">
+      <div className="header">
+        <h1 className="header-h">Bảng Xếp Hạng Podcast</h1>
+        <p className="header-p">Tất cả</p>
       </div>
-      <h2 className="menu-header">Cứ Chill Thôi</h2>
       <div className="main">
-        {data2.map((element) => (
-          <SubApp2
-            src={element.src}
-            title={element.title}
-            note={element.note}
-          />
-        ))}
+        <div className="component">
+          {data1.map((element) => (
+            <SubApp1
+              number={element.number}
+              src={element.src}
+              title={element.title}
+              chanel={element.chanel}
+              timeUp={element.timeUp}
+            />
+          ))}
+        </div>
+        <div className="component">
+          {data2.map((element) => (
+            <SubApp2
+              number={element.number}
+              src={element.src}
+              title={element.title}
+              chanel={element.chanel}
+              timeUp={element.timeUp}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
+
 export default App;
