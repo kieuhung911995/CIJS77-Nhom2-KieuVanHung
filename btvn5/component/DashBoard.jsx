13e39e8eb800727f233b9eebb8bc389efff89358
kieuhung911995 <110046293+kieuhung911995@users.noreply.github.com>
2022-11-01 14:17:06 +0700

    Delete btvn5 directory

--- btvn5/component/DashBoard.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const DashBoard = () => {
-  return <h1>Hello Admin!</h1>;
-};
-export default DashBoard;
--- btvn5/component/FailLogin.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const FailLogin = () => {
-  return <h1>Access Denied</h1>;
-};
-export default FailLogin;
