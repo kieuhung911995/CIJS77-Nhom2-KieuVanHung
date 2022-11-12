57645e632c4da69d60d9219067730c189c547daa
kieuhung911995 <110046293+kieuhung911995@users.noreply.github.com>
2022-11-12 14:07:35 +0700

    btvn9

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,60 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useState, createContext } from "react";
+
 import "./index.css";
-export default () => {
-  if (!localStorage.getItem("dataUser")) {
-    localStorage.setItem(
-      "dataUser",
-      JSON.stringify({ company: "", contact: "", country: "" })
-    );
-  }
-  const data = JSON.parse(localStorage.getItem("dataUser"));
-  const [value1, setValue1] = useState("");
-  const [value2, setValue2] = useState("");
-  const [value3, setValue3] = useState("");
-  console.log(value1);
-  console.log(localStorage.getItem("dataUser"));
-  console.log(data);
+import MainContent from "./MainContent";
 
-  useEffect(() => {
-    localStorage.setItem(
-      "dataUser",
-      JSON.stringify({ ...data, company: value1 })
-    );
-  }, [value1]);
-  useEffect(() => {
-    localStorage.setItem(
-      "dataUser",
-      JSON.stringify({ ...data, contact: value2 })
-    );
-  }, [value2]);
-  useEffect(() => {
-    localStorage.setItem(
-      "dataUser",
-      JSON.stringify({ ...data, country: value3 })
-    );
-  }, [value3]);
+export const ThemeChildContent = createContext();
+
+export default function App() {
+  const [themeChildContent, setThemeChildContent] = useState("");
+  const [themeOption, setThemeOption] = useState("");
+
+  const handleChangeTheme = (id) => {
+    if (id == 1) {
+      setThemeOption("white");
+      setThemeChildContent("white");
+    }
+    if (id == 2) {
+      setThemeOption("green");
+      setThemeChildContent("green");
+    }
+    if (id == 3) {
+      setThemeOption("blue");
+      setThemeChildContent("blue");
+    }
+  };
   return (
-    <>
-      <h2>HTML Table</h2>
-
-      <table>
-        <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
-        </tr>
-        <tr>
-          <td>
-            <input
-              type="text"
-              placeholder="Type your company's name here..."
-              value={data.company}
-              onChange={(e) => {
-                setValue1(e.target.value);
-              }}
-            ></input>
-          </td>
-          <td>
-            <input
-              type="text"
-              placeholder="Type your contact here..."
-              value={data.contact}
-              onChange={(e) => {
-                setValue2(e.target.value);
-              }}
-            ></input>
-          </td>
-          <td>
-            <input
-              type="text"
-              placeholder="Type your country..."
-              value={data.country}
-              onChange={(e) => {
-                setValue3(e.target.value);
-              }}
-            ></input>
-          </td>
-        </tr>
-      </table>
-    </>
+    <ThemeChildContent.Provider value={themeChildContent}>
+      <>
+        <div className="dropdown">
+          <button className="dropbtn">Change Theme ChildComponent</button>
+          <div className="dropdown-content">
+            <a
+              href="#"
+              onClick={() => handleChangeTheme(1)}
+              style={{ backgroundColor: themeOption }}
+            >
+              white (default)
+            </a>
+            <a
+              href="#"
+              onClick={() => handleChangeTheme(2)}
+              style={{ backgroundColor: themeOption }}
+            >
+              green
+            </a>
+            <a
+              href="#"
+              onClick={() => handleChangeTheme(3)}
+              style={{ backgroundColor: themeOption }}
+            >
+              blue
+            </a>
+          </div>
+        </div>
+        <MainContent />
+      </>
+    </ThemeChildContent.Provider>
   );
-};
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById("root")).render(<App />);
